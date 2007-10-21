16791354eee4e16555df7ec172e08f48b3d4885f
ssierr@law.columbia.edu <ssierr@law.columbia.edu@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-10-21 03:45:09 +0000

    * Added skeleton Interpreter class with I/O streams. * Added test directory. * Added InterpreterTest class with a "Hello, world!" test. * Added PhysicalcSuite class to hold all test classes. * Added 'test' target to Makefile which runs the suite.

---
+++ trunk/test/PhysicalcSuite.java
@@ -0,0 +1,17 @@
+package physicalc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+ 
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+    InterpreterTest.class
+    // add additional test classes here, separated by commas
+})
+
+public class PhysicalcSuite {
+    // the class remains completely empty, 
+    // being used only as a holder for the above annotations
+    // see http://radio.javaranch.com/lasse/2006/07/27/1154024535662.html
+}
