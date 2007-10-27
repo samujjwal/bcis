10e852b2cd784d23a083cad0f280b9d216ec16d5
ssierr@law.columbia.edu <ssierr@law.columbia.edu@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-10-27 02:29:26 +0000

    * added profile.sh, to set up CLASSPATH and other env vars.

---
+++ trunk/profile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# profile.sh
+
+# This file sets up the CLASSPATH and other needed environment
+# variables to run the example programs and tests.
+#
+# Do not execute this file as a shell script; instead, "source" it at
+# the shell command line like this:
+#
+#     source profile.sh
+#
+# This will only work if your shell is "bash".  It should work in any
+# UNIX-like environment, including Linux and Cygwin.
+
+
+# Java class search path: needs to include the project "class"
+# directory and any .jar files.
+export CLASSPATH=$PWD:$PWD/class:$PWD/lib/antlr.jar:$PWD/lib/junit-4.4.jar
+
+# Convenience alias for ANTLR.
+alias antlr="java antlr.Tool -diagnostic"
