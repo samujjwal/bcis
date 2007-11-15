a68b4ac6c4c31484ae433a197dfc8baa5286eb7b
brianwfoo <brianwfoo@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-15 21:14:02 +0000

    Added a file remotely

---
+++ trunk/src/physicalc/TypeError.java
@@ -0,0 +1,18 @@
+package physicalc;
+
+import java.lang.*;
+
+/** TypeError is raised when an operation cannot be performed because
+ * the types of its arguments are incompatible. */
+public class TypeError extends InterpreterError {
+
+    String errorMessage;
+
+    public TypeError(String operation, Datum object1, Datum object2) {
+		errorMessage = "Types are incompatible in the following operation:\n" + object1.toString() + " " + operation + " " + object2.toString() + "\n";
+    }
+
+    public String toString() {
+		return errorMessage;
+	}
+}
