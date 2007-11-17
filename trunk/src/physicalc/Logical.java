a190285a5df92dd2c1c2c0fad80102ba9765e19f
the.stuart.sierra <the.stuart.sierra@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-17 19:10:02 +0000

    Added Logical and Op base classes, and "And" example class. Updated Makefile.

---
+++ trunk/src/physicalc/Logical.java
@@ -0,0 +1,8 @@
+package physicalc;
+
+/** Logical is an abstract base class for all logical operator nodes.
+ *
+ * @see Node
+ */
+public abstract class Logical extends Expr  {
+}
---
+++ trunk/src/physicalc/Op.java
@@ -0,0 +1,8 @@
+package physicalc;
+
+/** Op is an abstract base class for all operator nodes.
+ *
+ * @see Node
+ */
+public abstract class Op extends Expr {
+}
