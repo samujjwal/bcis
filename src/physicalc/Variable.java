854752b038ccb4e904e6267d0ecb49850b2ee154
the.stuart.sierra <the.stuart.sierra@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-24 16:33:54 +0000

    Added templates for Id, Function, and Variable.

---
+++ src/physicalc/Variable.java
@@ -0,0 +1,28 @@
+package physicalc;
+
+
+/** A Variable stores a value locally.  Variable are created and
+ * modified with the "set" statement.  The name of the Variable is
+ * stored in the SymbolTable.  The value of a Variable can be changed.
+ *
+ * @see SymbolTable
+ * @see Set
+ */
+public class Variable implements RuntimeObject {
+
+    public Variable() {
+	;
+    }
+
+    public Variable(Datum initialValue) {
+	;
+    }
+
+    public Datum getValue() {
+	;
+    }
+
+    public Datum setValue(Datum newValue) {
+	;
+    }
+}
