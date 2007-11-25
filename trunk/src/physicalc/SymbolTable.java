a17f2778df58913c6e7665dd48c3aa45b51a32f4
the.stuart.sierra <the.stuart.sierra@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-25 23:57:45 +0000

    SymbolTable.java: added documentation.

--- trunk/src/physicalc/SymbolTable.java
+++ trunk/src/physicalc/SymbolTable.java
@@ -1,7 +1,6 @@
 package physicalc;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /** A SymbolTable associates symbols (strings) with run-time objects
  * (functions, variables, units, or constants).
@@ -11,20 +10,35 @@ import java.util.Map;
  * scopes in effect: a global scope for definitions and a local scope
  * for variables and function arguments.
  *
+ * SymbolTable stores any object that implements the RuntimeObject
+ * interface.  
+ *
  * @see RuntimeObject
  */
 public class SymbolTable {
 
-    private Map<String, RuntimeObject> table;
+    private HashMap<String, RuntimeObject> table;
 
+    /** Creates a new, empty symbol table. */
     public SymbolTable() {
 	table = new HashMap<String, RuntimeObject>();
     }
 
+    /** Associates an object with a symbol in the symbol table.  If
+     * "symbol" is already in the table, its value will be
+     * overwritten.
+     */
     public void put(String symbol, RuntimeObject object) {
 	table.put(symbol, object);
     }
 
+    /** Associates "newSymbol" with the value of "oldSymbol".  If
+     * "oldSymbol" is not defined, throws an UndefinedError.
+     *
+     * Aliases are not references.  If "oldSymbol" is redefined to
+     * point to a new object, the alias continues to point to the old
+     * object.
+     */
     public void putAlias(String newSymbol, String oldSymbol) {
 	if (table.containsKey(oldSymbol)) {
 	    table.put(newSymbol, table.get(oldSymbol));
@@ -33,6 +47,13 @@ public class SymbolTable {
 	}
     }
 
+    /** Looks up and returns the value of "symbol" in the table.
+     * Returns null if this table does not contain "symbol".
+     *
+     * Returns a generic RuntimeObject reference.  Callers of this
+     * method must check the type of the returned object and cast it
+     * appropriately.
+     */
     public RuntimeObject get(String symbol) {
 	return table.get(symbol);
     }
