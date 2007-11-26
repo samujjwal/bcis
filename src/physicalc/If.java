a8368804e62bc3e80d1ddc5078948358c4c43bcb
ChadJiang <ChadJiang@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-26 03:52:33 +0000

    modify If.java

--- src/physicalc/If.java
+++ src/physicalc/If.java
@@ -1,13 +1,32 @@
 package physicalc;
 
+/** this is If Class
+ *  Syntax: if expression1 then
+ *             statements1
+ *          elsif expression2 then
+ *             statements2
+ *			else
+ *			   statements3
+ *			done
+ */
 public class If extends Stmt {
 
+	private Expr expr1;
+	private Block block1;
+	private Block block2;
     public If(Expr condition, Block thenBlock, Block elseBlock) {
-        ;
-    }
+		expr1 = condition;
+		block1 = thenBlock;
+		block2 = elseBlock;
+	}
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
-        return null; // remove
-    }
+        if (expr1.eval(globals, locals).isTrue()) {
+			return block1.eval(globals,locals);
+		}
+		else { 
+			return block2.eval(globals,locals);
+        }
+		
 }
 
