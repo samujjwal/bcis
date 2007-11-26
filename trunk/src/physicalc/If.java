231322031fcb377822335166d627ec840e69858f
the.stuart.sierra <the.stuart.sierra@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-26 23:32:30 +0000

    * Fixed missing brace in If.java

--- trunk/src/physicalc/If.java
+++ trunk/src/physicalc/If.java
@@ -11,22 +11,24 @@ package physicalc;
  */
 public class If extends Stmt {
 
-	private Expr expr1;
-	private Block block1;
-	private Block block2;
+    private Expr expr1;
+    private Block block1;
+    private Block block2;
     public If(Expr condition, Block thenBlock, Block elseBlock) {
-		expr1 = condition;
-		block1 = thenBlock;
-		block2 = elseBlock;
-	}
+	expr1 = condition;
+	block1 = thenBlock;
+	block2 = elseBlock;
+    }
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
         if (expr1.eval(globals, locals).isTrue()) {
-			return block1.eval(globals,locals);
-		}
-		else { 
-			return block2.eval(globals,locals);
+	    return block1.eval(globals,locals);
+	}
+	else { 
+	    return block2.eval(globals,locals);
         }
 		
+    }
 }
+
 
