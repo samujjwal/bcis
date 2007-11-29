7a96115d2ce8dc798c9134db1e12bb6207e3e217
ChadJiang <ChadJiang@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-29 14:39:11 +0000

    While class

--- src/physicalc/While.java
+++ src/physicalc/While.java
@@ -1,13 +1,34 @@
 package physicalc;
 
+import java.lang.*;
+
+/** While Statement
+ *  While expr1 do
+ *        block1
+ *  done
+ */
+
 public class While extends Stmt {
 
+	private Expr expr1;
+	private Block block1;
+
+	public While() { expr1 = null; block1 =null;}
+
     public While(Expr testExpr, Block block) {
-        ;
+        expr1 = testExpr;
+		block1 = block;
     }
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
-        return null; // remove
+        if (expr1.eval(globals,locals).isTrue())
+         	return block1.eval(globals,locals);
+	/* this is for test purpose */
+	//	else
+	//		System.err.println("Calling eval() in Block");
+    return null;
     }
+
+	
 }
 
