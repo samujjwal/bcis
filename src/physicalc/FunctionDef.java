8da1351454665127cc801a81a401add777a5de08
brianwfoo <brianwfoo@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-11-30 23:21:38 +0000

    Added a file remotely

---
+++ src/physicalc/FunctionDef.java
@@ -0,0 +1,23 @@
+package physicalc;
+
+public class FunctionDef extends Def {
+
+	private String id;
+	private ParamList paramList;
+	private Block bodyBlock;
+
+    public FunctionDef(String i, ParamList pl, Block bb) {
+        id = i;
+        paramList = pl;
+        bodyBlock = bb;
+    }
+
+    public Datum eval(SymbolTable globals, SymbolTable locals) {
+
+		RuntimeObject func = new Function(paramList,bodyBlock);
+        globals.put(id,func);
+
+        return null; // definitions always return null
+    }
+}
+
