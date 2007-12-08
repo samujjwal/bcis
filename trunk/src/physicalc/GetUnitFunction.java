8aba2288f4f2136de7919059fe08925b28e4e8db
brianwfoo <brianwfoo@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 18:16:37 +0000

    Added a file remotely

---
+++ trunk/src/physicalc/GetUnitFunction.java
@@ -0,0 +1,26 @@
+package physicalc;
+
+public class GetUnitFunction extends Function {
+
+    public GetUnitFunction() { ; }
+
+    public Datum call(SymbolTable globals, SymbolTable locals, ExprList arguments) {
+
+		System.err.println("Calling call() in GetUnitFunction");
+
+		if (arguments.getContents().size() != 1) {
+			throw new InterpreterError("Cannot call getUnit on more than one argument");
+		}
+
+		Expr expr = arguments.getContents().get(0);
+
+		Datum pair = expr.eval(globals,locals);
+
+		if (pair instanceof PUnitPair) {
+			return ((PUnitPair)pair).getUnit();
+		} else {
+			throw new InterpreterError("Must call GetUnitFunction on a UnitPair");
+		}
+    }
+
+}
