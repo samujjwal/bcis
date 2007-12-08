9f06a30f5eeca15402a0808b5626d24bc4daecfa
digitalfobulous <digitalfobulous@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 18:25:26 +0000

    variable and constant

--- trunk/src/physicalc/AliasDef.java
+++ trunk/src/physicalc/AliasDef.java
@@ -2,13 +2,28 @@ package physicalc;
 
 public class AliasDef extends Def {
 
+    String newSymb;
+    String oldSymb;
+
     public AliasDef(String newSymbol, String oldSymbol) {
-        ;
+        newSymb = newSymbol;
+	oldSymb = oldSymbol;
     }
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
-        // TODO
-        return null; // definitions always return null
+        //lookup old symbol in global symbol table
+	//then, if it's not defined, throw an error
+	//add new entry to global symbol table with newSymb as the symbol
+	//value as value
+
+	RuntimeObject R = globals.get(oldSymb);
+	
+	if(R == null) {
+		throw new UndefinedError(oldSymb);
+	}
+	else {
+		globals.put(newSymb, R);
+	}
     }
 }
 
