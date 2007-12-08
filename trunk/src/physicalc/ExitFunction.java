6632573772a8c54b7afe060c656a93249fa2d3e8
ChadJiang <ChadJiang@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 20:26:52 +0000

    fix bug

--- trunk/src/physicalc/ExitFunction.java
+++ trunk/src/physicalc/ExitFunction.java
@@ -2,12 +2,12 @@ package physicalc;
 
 public class ExitFunction extends Function {
 
-    public ExitFucntion() { ; }
+    public ExitFunction() { ; }
     
-    public Datum call(SymbolTable globals, SymbolTable locals, ExprList 
-arguments) {
-    //System.out.println("Calling call() in ExitFunction");
-    System.exit(0);
-    }
-    return null;
+    public Datum call(SymbolTable globals, SymbolTable locals, ExprList arguments) {
+//      System.err.println("Calling call() in ExitFunction");
+      System.exit(0);
+      return null;  
+  }
+   
 }
