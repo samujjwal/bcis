4a59fd0a2a487f0a7ca18adb2d4070acfc610d41
digitalfobulous <digitalfobulous@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 17:03:09 +0000

    variable and constant

---
+++ src/physicalc/ToStringFunction.java
@@ -0,0 +1,18 @@
+package physicalc;
+
+public class ToStringFunction extends Function {
+
+    public ToStringFunction() { ; }
+
+    public Datum call(SymbolTable globals, SymbolTable locals, ExprList arguments) {
+
+		Datum number = arguments.eval(globals,locals);
+
+		if (number instanceof PNumber) {
+			return new PNumber( ((PNumber)number).toString() );
+		} else {
+			throw new InterpreterError("Cannot call ToString function on non-number");
+		}
+    }
+
+}
