7e378e7f8c2a3735ada34805fe16f927fc17d031
the.stuart.sierra <the.stuart.sierra@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 18:17:33 +0000

    Tests for Constant. Load.java implemented.

--- src/physicalc/Load.java
+++ src/physicalc/Load.java
@@ -1,6 +1,9 @@
 package physicalc;
 
 import java.lang.String;
+import java.io.*;
+import antlr.CommonAST;
+import antlr.collections.AST;
 
 /** Load implements the "load" statement.
  * 
@@ -8,11 +11,39 @@ import java.lang.String;
  */
 public class Load extends Node {
 
+    private String file;
+
     public Load(String filename) {
-	;
+	file = filename;
     }
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
-	return null; // replace
+	Reader input;
+	try {
+	    input = new FileReader(file);
+	} catch (FileNotFoundException e) {
+	    System.err.println("File '" + file + "' not found.");
+	    System.exit(1);
+	    return null; // to keep the compiler happy
+	}
+	
+	try {
+	    PhysiLexer lexer = new PhysiLexer(input);
+	    PhysiParser parser = new PhysiParser(lexer);
+
+	    parser.program();
+
+	    CommonAST parseTree = (CommonAST)parser.getAST();
+
+	    PhysiWalker walker = new PhysiWalker();
+	    Program p = walker.program(parseTree);
+
+	    p.eval(globals, locals);
+	
+	} catch(Exception e) {
+	    System.err.println(e.toString());
+	}
+
+	return null;
     }
 }
