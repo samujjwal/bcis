d2f999f67dcdde56037923f9513ea2109b6a52c0
digitalfobulous <digitalfobulous@9e34c554-683a-0410-9a2f-4bb5436fbba4>
2007-12-08 18:38:45 +0000

    variable and constant

--- src/physicalc/ConstantDef.java
+++ src/physicalc/ConstantDef.java
@@ -2,13 +2,22 @@ package physicalc;
 
 public class ConstantDef extends Def {
 
+    private String id1;
+    private Expr valueExpr1;
+ 
     public ConstantDef(String id, Expr valueExpr) {
-        ;
+        id1 = id;
+	valueExpr1 = valueExpr;
     }
 
     public Datum eval(SymbolTable globals, SymbolTable locals) {
-        // TODO
-        return null; // definitions always return null
+	Constant c1 = new Constant(valueExpr1.eval(globals, locals));
+	
+	RuntimeObject R = c1;
+	
+	globals.put(id1, R);
+
+        return null;
     }
 }
 
